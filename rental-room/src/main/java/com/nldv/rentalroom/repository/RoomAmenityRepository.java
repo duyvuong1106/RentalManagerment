0b6e888fba9804bb1a90f94b28dc9bad022dab4f
duyvuong1106 <duyvuong1106@gmail.com>
2026-09-11 17:17:56 +0700

    fix RoomAmenityRepository

--- rental-room/src/main/java/com/nldv/rentalroom/repository/RoomAmenityRepository.java
+++ rental-room/src/main/java/com/nldv/rentalroom/repository/RoomAmenityRepository.java
@@ -3,5 +3,6 @@ package com.nldv.rentalroom.repository;
 import com.nldv.rentalroom.pojo.RoomAmenity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomAmenityRepository extends JpaRepository<RoomAmenity, Integer> {
+public interface RoomAmenityRepository
+        extends JpaRepository<RoomAmenity, RoomAmenity.RoomAmenityId> {
 }
