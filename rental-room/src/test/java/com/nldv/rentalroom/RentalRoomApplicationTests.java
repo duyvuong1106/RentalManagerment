88477ecf98d05bec57a141c4e2e9d6d4b55fcf84
duyvuong1106 <duyvuong1106@gmail.com>
2026-09-11 14:03:16 +0700

    project

---
+++ rental-room/src/test/java/com/nldv/rentalroom/RentalRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nldv.rentalroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
